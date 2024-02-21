45d1e83b378c436ef6bde069181818b028ab53d3
sasanktumpati <sasanktumpati475@gmail.com>
2024-02-22 01:14:45 +0530

    1

--- lib/ui/homelistview.dart
+++ lib/ui/homelistview.dart
@@ -1,46 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:movie_search/models/nowplaying.dart';
+import 'package:movie_search/ui/searchbox.dart';
+import 'package:provider/provider.dart';
 
-class MovieCard extends StatelessWidget {
-  final NowPlaying movie;
+import '../models/bytitle.dart';
+import '../models/nowplaying.dart';
+import 'moviecard.dart';
 
-  const MovieCard({Key? key, required this.movie}) : super(key: key);
+class HomeListView extends StatefulWidget {
+  @override
+  _HomeListViewState createState() => _HomeListViewState();
+}
 
+class _HomeListViewState extends State<HomeListView> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: ListTile(
-        contentPadding: EdgeInsets.all(8),
-        leading: Image.network(
-          movie.poster,
-          width: 100,
-          height: 150,
-          fit: BoxFit.cover,
-        ),
+    return Scaffold(
+      appBar: AppBar(
         title: Text(
-          movie.title,
+          'Movie App',
           textAlign: TextAlign.center,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          ),
         ),
-        subtitle: Text(
-          'Release Year: ${movie.year}',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 14,
-          ),
-        ),
-        onTap: () {
-          // Navigate to movie details page
-          // You can implement this based on your navigation setup
-        },
+        centerTitle: true,
       ),
+      body: Column(
+        children: [
+          SearchBox(),
+          Expanded(
+            child: Consumer<HomeListProvider>(
+              builder: (context, provider, _) {
+                if (provider.isLoading) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
+                  return provider.searchedMovies.isEmpty
+                      ? _buildNowPlayingList(provider.nowPlayingMovies)
+                      : _buildSearchedList(provider.searchedMovies);
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNowPlayingList(List<NowPlaying> nowPlayingMovies) {
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: MovieCard(movie: movie),
+        );
+      },
+    );
+  }
+
+  Widget _buildSearchedList(List<MovieResult> searchedMovies) {
+    return ListView.builder(
+      itemCount: searchedMovies.length,
+      itemBuilder: (context, index) {
+        final movie = searchedMovies[index];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: MovieCard(movie: movie),
+        );
+      },
     );
   }
 }
